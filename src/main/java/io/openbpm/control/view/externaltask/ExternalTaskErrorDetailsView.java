f581b25996a949fe2ca48725d40a7603a43e34b7
Ilia Grachev <i.grachev@haulmont.net>
2025-03-20 20:42:42 +0400

    Add a 'copy to clipboard' button to the stacktrace dialog in the incident details screen #12

--- src/main/java/io/openbpm/control/view/externaltask/ExternalTaskErrorDetailsView.java
+++ src/main/java/io/openbpm/control/view/externaltask/ExternalTaskErrorDetailsView.java
@@ -8,7 +8,13 @@ package io.openbpm.control.view.externaltask;
 
 import com.vaadin.flow.router.Route;
 import io.jmix.flowui.component.codeeditor.CodeEditor;
-import io.jmix.flowui.view.*;
+import io.jmix.flowui.view.DialogMode;
+import io.jmix.flowui.view.StandardView;
+import io.jmix.flowui.view.Subscribe;
+import io.jmix.flowui.view.ViewComponent;
+import io.jmix.flowui.view.ViewController;
+import io.jmix.flowui.view.ViewDescriptor;
+import io.openbpm.control.action.CopyComponentValueToClipboardAction;
 import io.openbpm.control.service.externaltask.ExternalTaskService;
 import io.openbpm.control.view.main.MainView;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,12 +22,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 @Route(value = "external-task-error-details", layout = MainView.class)
 @ViewController("ExternalTaskErrorDetailsView")
 @ViewDescriptor("external-task-error-details-view.xml")
-@DialogMode(width = "35em")
+@DialogMode(width = "52em")
 public class ExternalTaskErrorDetailsView extends StandardView {
     @Autowired
     protected ExternalTaskService externalTaskService;
     @ViewComponent
     protected CodeEditor errorDetailsCodeEditor;
+    @ViewComponent
+    protected CopyComponentValueToClipboardAction copy;
 
     protected String externalTaskId;
     protected boolean fromHistory = false;
@@ -43,5 +51,6 @@ public class ExternalTaskErrorDetailsView extends StandardView {
             stacktrace = externalTaskService.getErrorDetails(externalTaskId);
         }
         errorDetailsCodeEditor.setValue(stacktrace);
+        copy.setTarget(errorDetailsCodeEditor);
     }
 }
